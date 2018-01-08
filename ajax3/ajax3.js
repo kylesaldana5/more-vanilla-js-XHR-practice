b7fa649beb0109b560de32d2578675586cf396c9
kylesaldana5 <kylesaldana5@gmail.com>
2018-01-07 21:53:58 -0600

    finished ajax3

--- ajax3/ajax3.js
+++ ajax3/ajax3.js
@@ -10,8 +10,20 @@ function loadusers() {
         if (this.status === 200) {
             var users = JSON.parse(this.responseText)
 
-            console.log('',users );
+            var output = '' ;
             
+            for(var i in users){
+              output += 
+              '<div class="user">'+
+              '<img src="'+users[i].avatar_url+'" width="70" height="70"'+
+              '<ul>' +
+              '<li>ID: '+users[i].id+'</li>'+
+              '<li>ID: '+ users[i].login+ '</li>'+
+              '</ul>'+
+              '</div>'
+                
+            }
+            document.getElementById("users").innerHTML = output;
         }
     }
     xhr.send();
